e9e5f48f5e39d759c6bb0c307c16227b6d155982
ev0clu <ev0clu@gmail.com>
2024-03-02 19:03:51 +0100

    PORT enivornment variable usage is added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,10 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(4000);
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
+
+  await app.listen(PORT);
 }
 bootstrap();
