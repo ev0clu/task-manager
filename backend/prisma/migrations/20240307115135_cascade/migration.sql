c10449666e22aa9cad3a59329024f50b9eee6772
ev0clu <ev0clu@gmail.com>
2024-03-07 13:46:39 +0100

    Cascade delete action is added

---
+++ backend/prisma/migrations/20240307115135_cascade/migration.sql
@@ -0,0 +1,29 @@
+-- DropForeignKey
+ALTER TABLE "Activity" DROP CONSTRAINT "Activity_workspaceId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Board" DROP CONSTRAINT "Board_workspaceId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Card" DROP CONSTRAINT "Card_listId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "List" DROP CONSTRAINT "List_boardId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Workspace" DROP CONSTRAINT "Workspace_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Workspace" ADD CONSTRAINT "Workspace_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Board" ADD CONSTRAINT "Board_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Activity" ADD CONSTRAINT "Activity_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "List" ADD CONSTRAINT "List_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Card" ADD CONSTRAINT "Card_listId_fkey" FOREIGN KEY ("listId") REFERENCES "List"("id") ON DELETE CASCADE ON UPDATE CASCADE;
