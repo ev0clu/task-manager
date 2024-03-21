aaf93bbf7bc20b58612067d2ea83df1a673e8b67
ev0clu <ev0clu@gmail.com>
2024-03-21 15:55:30 +0100

    Header component is updated

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,81 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
+import logoSrc from '../assets/logo.png';
+import {
+  Box,
+  Divider,
+  IconButton,
+  Stack,
+  Typography
+} from '@mui/material';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const Header = () => {
-  const { toggleTheme } = useContext(ThemeContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
   return (
-    <div>
-      <button onClick={toggleTheme}>toggle</button>
-    </div>
+    <Stack
+      direction={'row'}
+      spacing={1}
+      justifyContent={'space-between'}
+      sx={{ padding: '0.5rem' }}
+    >
+      <Stack direction={'row'} spacing={1}>
+        <Box
+          component="img"
+          sx={{
+            height: '2rem',
+            width: 'auto'
+          }}
+          src={logoSrc}
+          alt="logo-img"
+        />
+        <Typography variant="h5" sx={{ fontWeight: 600 }}>
+          Task Manager
+        </Typography>
+      </Stack>
+      <Stack
+        direction={'row'}
+        spacing={1}
+        divider={<Divider orientation="vertical" flexItem />}
+      >
+        <IconButton
+          aria-label="logout"
+          size="medium"
+          color="primary"
+          onClick={toggleTheme}
+        >
+          <SettingsIcon />
+        </IconButton>
+        <IconButton
+          aria-label="logout"
+          size="medium"
+          color="primary"
+          onClick={toggleTheme}
+        >
+          <LoginIcon />
+        </IconButton>
+        <IconButton
+          aria-label="logout"
+          size="medium"
+          color="primary"
+          onClick={toggleTheme}
+        >
+          <LogoutIcon />
+        </IconButton>
+        <IconButton
+          aria-label="toggle theme"
+          size="medium"
+          color="primary"
+          onClick={toggleTheme}
+        >
+          {theme === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
+        </IconButton>
+      </Stack>
+    </Stack>
   );
 };
 
