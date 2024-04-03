4e4df18b7ab3e0e8f34d268b6235f1637c27cb85
ev0clu <ev0clu@gmail.com>
2024-04-03 16:53:50 +0200

    CORS is added to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
