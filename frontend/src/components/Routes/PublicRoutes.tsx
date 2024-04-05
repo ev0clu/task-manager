46631768ba63d32df30e4e954ad02343f2c37e0b
ev0clu <ev0clu@gmail.com>
2024-04-05 09:28:12 +0200

    Logged in state is handled

--- frontend/src/components/Routes/PublicRoutes.tsx
+++ frontend/src/components/Routes/PublicRoutes.tsx
@@ -1,6 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContextProvider';
 
 const PublicRoutes = () => {
+  const { token } = useAuth();
+
+  // Check if the user is authenticated
+  if (token) {
+    // If not authenticated, redirect to the login page
+    return <Navigate to="/dashboard" />;
+  }
   return <Outlet />;
 };
 
