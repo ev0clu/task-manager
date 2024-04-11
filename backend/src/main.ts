12e59aa608dd0a58927ec72fd5e1296348b7a3f8
ev0clu <ev0clu@gmail.com>
2024-04-11 14:13:16 +0200

    Production address is added to cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:4173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
